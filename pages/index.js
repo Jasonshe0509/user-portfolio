679237a119d03de76148a91b524b84bd4ab046e6
Jasonshe0509 <jasonshe80@gmail.com>
2023-12-08 21:09:10 +0800

    feat: initial project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home(){
+  return(
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+    </div>
+  );
+}
