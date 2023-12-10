72766693fef8f8226c02472eaa86552472c2f7e6
Jasonshe0509 <jasonshe80@gmail.com>
2023-12-10 17:55:05 +0800

    feat:user profile

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,55 +1,87 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
+import TypeWritter from "typewriter-effect";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
 
 const HeroSection = () => {
-  // JS in here
   return (
-    // HTML CSS in return
-    <main className="py-16 mt-16 md:flex md: shirink-0">
+    <main className=" mx-10 md-flex md:shrink-0 ">
       <div className="grid grid-cols-1 md:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-1 md:col-span-8 place-self-center justify-self-start"
-        >
-          <div className="col-span-1 md:col-span-8 place-self-center justify-self-start">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-1 md:col-span-8 place-self-center justify-self-start"
+          >
             <h1 className="font-extrabold">
-              <span className="text-white text-8xl">
-                Hello, I'm{" "}
+              {/* span text seperator -- for design*/}
+              <span className="text-white text-8xl ">
+                Hello, I'm{" "} 
                 <span className="text-transparent bg-clip-text sm:text-8xl md:text-9xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-                    Jun Yuan
+                Jason She 
                 </span>
               </span>
-              <span className="text-7xl">
-                <Typewriter
-                    options={{
+              <br></br>
+              <span className="text-7xl">I'm 
+              <TypeWritter
+                options={{
                     autoStart: true,
                     loop: true,
-                    }}
-                    onInit={(typewriter) => {
-                    typewriter.typeString('<span class="text-white">Web Developer</span>')
-                        .pauseFor(1500)
-                        .deleteAll()
-                        .typeString('<span class="text-white">Student</span>')
-                        .pauseFor(1500)
-                        .deleteAll()
-                        .start();
-                    }}
-                />
-                </span>
-                <p className="text-sky-100 mt-8 font-bold text-based sm:text-lg md:text-xl mb-6">Slide down to know more about me</p>
-                <Link href="https://drive.google.com/file/d/1rDFhgt8sZ7sEixg44f5Xw99TftY8dKeX/view?usp=sharing" 
-                    className="px-6 inline-block py-3 rounded-full border border-solid-2 border-pink-500 hover:bg-pink-400" target="_blank">
-                View My Resume
-                </Link>
+                }}
+                onInit={(typewriter) => {
+                    typewriter
+                    .typeString`<span style="color:#FF77FF">Software Engineer</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .typeString`<span style="color:#9E7BFF">Back-End Developer</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .typeString`<span style="color:#893BFF">Data Analyst</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .start();
+
+                }}
+              ></TypeWritter>
+              </span>
             </h1>
+            <p className="text-violet-400	font-bold pt-8 text-base sm:text-lg mb-6 lg:text-xl">
+              Slide down to know more about me! 
+            </p>
+              <Link
+                href="#projects" 
+                className="px-12 mb-4 font-bold inline-block py-3 rounded-full mr-4 border-solid border-2 border-indigo-600 hover:bg-violet-600">
+                <span className="text-xl text-white">Projects</span>
+              </Link>
+              <Link
+                href="https://drive.google.com/file/d/1rDFhgt8sZ7sEixg44f5Xw99TftY8dKeX/view?usp=drive_link"
+                className="px-6 font-bold inline-block py-3 rounded-full mr-4 border-solid border-2  border-pink-500 hover:bg-pink-400"
+                target="_blank">
+                <span className="text-xl text-white">View my Resume</span>
+              </Link>
+          </motion.div>
+        <div className="col-span-4 md:col-span-4 place-self-center mt-12 lg:mt-0 pt-0 lg:pt-24">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+          <div className="w-[400px] h-[400px] relative z-0">
+            <img
+              src="/Jason.jpg"
+              alt="Jason"
+              className=" transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/2"
+              width={450}
+              height={450}
+            />
           </div>
-        </motion.div>
+          </motion.div>
+        </div>
       </div>
     </main>
   );
 };
-export default HeroSection;
+
+export default dynamic(() => Promise.resolve(HeroSection), {ssr: false});
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import HeroSection from "@/components/HeroSection";
+import AchievementsSection from "@/components/AchievementsSection";
+import AboutMe from "@/components/AboutMe";
+import Project from "@/components/Project";
+import EmailSection from "@/components/EmailSection";
+import Navbar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 export default function Home(){
   return(
     <div className="flex min-h-screen flex-col">
-      <HeroSection />
+      <main>
+        <Navbar/>
+        <HeroSection />
+        <AchievementsSection/>
+        <AboutMe/>
+        <Project/>
+        <EmailSection/>
+      </main>
+      <Footer/>
     </div>
   );
 }
